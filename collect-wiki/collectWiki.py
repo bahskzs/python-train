380799f885fe7fd9ec8485d12982d6b8acc5a286
bahskzs <bahskzs610@live.com>
2021-05-31 12:01:54 +0800

    2021-05-31 1.append记录优化，每天处理的时候都把当天相关记录清除后再追加内容

--- collect-wiki/collectWiki.py
+++ collect-wiki/collectWiki.py
@@ -6,13 +6,19 @@ from log import LogUtil
 import time
 
 # 读取文件
-df = pd.read_csv("H:\software\My Research Folder.csv")
+df = pd.read_csv("H:\Downloads\My Research Folder.csv")
 # 找出是今天收集的
-data = df[df["Date"] == str(datetime.date.today())]
-data["collect_date"] = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+today=str(datetime.date.today())
+data = df[df["Date"] == today]
+collectDate = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+data["collect_date"] = collectDate
 
 try:
     engine = HandleMysql().conn_mysql()
+    # 每天处理的时候都把当天相关记录清除后再追加内容
+    engine.execute("delete from t_collect where Date = '{collectDate}'".format(
+        collectDate=str(datetime.date.today())
+    ))
     data.to_sql(name='t_collect', con=engine, if_exists='append', index=False, index_label=False)
     LogUtil.info(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + " Success write to database ," + str(
         len(data)) + " records")
